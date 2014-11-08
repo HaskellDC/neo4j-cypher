7f07ac5273200e68b2535028742c16b3e408779b
Wes Freeman <wes@skeweredrook.com>
2014-11-08 15:57:49 -0500

    adding new quasiquoter tests. converting db tests to use quasiquoter

---
+++ tests/Language/Cypher/quasiquoter-test-suite.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
+import Test.Tasty.HUnit ((@?=), Assertion, testCase)
+import Test.Tasty.TH (defaultMainGenerator)
+
+import Language.Cypher
+import Language.Cypher.Quasiquoter
+
+case_simpleMatchExample :: Assertion
+case_simpleMatchExample = 
+  writeQuery [cypher| MATCH (n) RETURN n|] @?= "MATCH (n) RETURN (n)"
+
+main :: IO ()
+main = $defaultMainGenerator
